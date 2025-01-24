3f267165235b4741a8517cbe6fb1124525b8a9ae
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-01-24 08:58:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request, redirect, session, g, flash
 import sqlite3
+import os
 
 app = Flask(__name__)
-app.secret_key = "your_secret_key"
+app.secret_key = os.urandom(24)
 DATABASE = "database.db"
 
 def get_db():
