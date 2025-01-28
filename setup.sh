dde9a4d19bbea3c543fec3ddcc9070cdeb79b2a1
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-01-28 23:43:46 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,47 @@
 #!/bin/bash
 
 cd
-apt update -y
-apt upgrade -y
-apt install git -y
+apt update 
+sudo apt install git
 
-git clone 
+git clone https://github.com/Sandhj/project.git
+
+cd project
+python3 -m venv web
+source web/bin/activate
+
+pip install flask
+pip install requests
+pip install paramiko
+deactivate
+
+cd
+cat <<EOL > /etc/systemd/system/app.service
+[Unit]
+Description=Run project script
+After=network.target
+
+[Service]
+ExecStart=/bin/bash /root/project/run.sh
+WorkingDirectory=/root/project
+User=root
+Group=root
+Restart=always
+StandardOutput=journal
+StandardError=journal
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+# Reload systemd dan aktifkan service
+systemctl daemon-reload
+systemctl enable app.service
+
+# Menjalankan service
+systemctl start app.service
+
 
 # Prompt untuk token bot Telegram dan chat ID
 echo "Masukkan Token Telegram Anda (didapat dari BotFather): "
