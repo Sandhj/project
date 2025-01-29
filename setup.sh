1f80a1f2966367840c5c6731bab706a3609f7249
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-01-29 11:14:22 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -218,6 +218,30 @@ else:
 
 EOL
 
+# Setup Nginx
+sudo apt update
+sudo apt install nginx
+
+cat <<EOL > /etc/nginx/sites-available/web.easyvpn.biz.id
+server {
+    listen 80;
+    server_name web.easyvpn.biz.id;
+
+    location / {
+        proxy_pass http://143.198.88.146:5003;  # Ganti dengan alamat Flask Anda
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+}
+
+EOL
+
+sudo ln -s /etc/nginx/sites-available/web.easyvpn.biz.id /etc/nginx/sites-enabled/
+sudo nginx -t
+sudo systemctl restart nginx
+
 cd
 rm -r setup.sh
 
