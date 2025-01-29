b2dce57febd1935fd4a800e03cbbe448194efd7e
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-01-29 12:33:42 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -231,14 +231,13 @@ server {
     server_name web.easyvpn.biz.id;
 
     location / {
-        proxy_pass http://$ip:5003;  # Ganti dengan alamat Flask Anda
+        proxy_pass http://$ip:5003;  # Pastikan ini adalah alamat Flask Anda
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto $scheme;
     }
 }
-
 EOL
 
 sudo ln -s /etc/nginx/sites-available/web.easyvpn.biz.id /etc/nginx/sites-enabled/
